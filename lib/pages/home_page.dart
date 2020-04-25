72969bb2d1fcd674ea13783dfd6670e3db012431
shrimant <nikate.shri@gmail.com>
2020-04-25 15:43:21 +0530

    WIP: Background Clipper implementation.

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gin_app/wigets/custom_widget.dart';
 
@@ -21,19 +22,49 @@ class _MyHomePageState extends State<MyHomePage> {
         Container(
           color: Theme.of(context).primaryColor,
         ),
-        Center(
-          child: Column(
-            children: <Widget>[
-              Center(
-                child: CustomStepper(
-                  numberOfSteps: _numberOfSteps,
-                  currentStep: _currentStep,
-                ),
+        CustomStepper(
+          numberOfSteps: _numberOfSteps,
+          currentStep: _currentStep,
+        ),
+        Positioned(
+          bottom: 0,
+          right: 0,
+          left: 0,
+          top: 100,
+          child: ClipPath(
+            clipper: BackgroundClipper(),
+            child: Container(
+              decoration:
+                  BoxDecoration(color: Colors.grey, shape: BoxShape.rectangle),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Center(),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ]),
     );
+  }
+}
+
+class BackgroundClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    var controllPoint = Offset(50, size.width);
+    var endPoint = Offset(size.height / 2, size.width);
+    path.quadraticBezierTo(
+        controllPoint.dx, controllPoint.dy, endPoint.dx, endPoint.dy);
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 0);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return true;
   }
 }
